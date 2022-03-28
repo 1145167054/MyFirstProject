80b21a6450ad5e7c88e637e5215b940564e841cf
mx <mx1943563308@163.com>
2022-03-28 16:51:59 +0800

    added a test

--- hello.c
+++ hello.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int mutiply(int a ,int b){
-<<<<<<< HEAD
 	return (a*b);
-=======
+}
 	return a*b;
 >>>>>>> main
 }
 int sum(int a,int b){
 	return (a+b);
+}
+int test(){
+	return 0;
 }
 int main(){
 	printf("hello world");
